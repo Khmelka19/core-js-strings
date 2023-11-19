026d0f791b19f115796687094dbd57fecadb8758
Oxana Savianok <oxanas8833@gmail.com>
2023-11-19 14:52:34 +0300

    feat: add the solution to get the card

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -496,8 +496,11 @@ function encodeToRot13(str) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const str =
+    "'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'";
+  const arr = str.split(',').map((el) => el.slice(1, -1));
+  return arr.indexOf(value);
 }
 
 module.exports = {
